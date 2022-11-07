dce0e5d00b032ae9ed46b2afaa5931f284f6ded3
JustinRWong <justin@joinneon.com>
2022-11-07 04:19:57 -0800

    try and print exception

--- viz.py
+++ viz.py
@@ -35,50 +35,56 @@ def disasters():
 
 @viz_page.route('/search', methods=['GET', 'POST'])
 def search():
-    args = request.args
-    width, height = determine_chart_dimensions(args)
-    if request.method == 'GET':
-        return render_template("/search.html", title="Search")
-    if request.method == 'POST':
-        name = request.form['text']
-        json_chart = make_search_chart(raw_data=DATASET,
-                                        width=width,
-                                        height=height,
-                                        incident=name.lower(),
-                                        json=True)
-        return render_template("/search.html",
-                        title="Search",
-                        input=name,
-                        chart_json=json_chart
-                        )
+    try:
+        args = request.args
+        width, height = determine_chart_dimensions(args)
+        if request.method == 'GET':
+            return render_template("/search.html", title="Search")
+        if request.method == 'POST':
+            name = request.form['text']
+            json_chart = make_search_chart(raw_data=DATASET,
+                                            width=width,
+                                            height=height,
+                                            incident=name.lower(),
+                                            json=True)
+            return render_template("/search.html",
+                            title="Search",
+                            input=name,
+                            chart_json=json_chart
+                            )
+    except Exception as e:
+        return "Something went wrong: " + e + e.message
 
 @viz_page.route('/deepdive', methods=['GET', 'POST'])
 def deepdive():
-    args = request.args
-    width, height = determine_chart_dimensions(args)
-    domain = [args.get('begin_date','1979-01-01'), args.get('end_date','2022-11-01')]
-    disaster_types = get_unique_disaster_types()
-    if request.method == 'GET':
-        json_chart = make_deepdive_chart(domain=domain,
-                                        width=width,
-                                        height=height,
-                                        disaster_type='All',
-                                        json=True
-                                        )
-        return render_template("/deepdive.html",
-                                title="Deepdive",
-                                disaster_types=disaster_types,
-                                chart_json=json_chart)
-    if request.method == 'POST':
-        disaster_type = request.form['disaster_type']
-        json_chart = make_deepdive_chart(domain=domain,
-                                        width=width,
-                                        height=height,
-                                        disaster_type=disaster_type,
-                                        json=True
-                                        )
-        return render_template('/deepdive.html',
-                                title='Deepdive',
-                                disaster_type=disaster_type,
-                                disaster_types=disaster_types,
-                                chart_json=json_chart)
+    try:
+        args = request.args
+        width, height = determine_chart_dimensions(args)
+        domain = [args.get('begin_date','1979-01-01'), args.get('end_date','2022-11-01')]
+        disaster_types = get_unique_disaster_types()
+        if request.method == 'GET':
+            json_chart = make_deepdive_chart(domain=domain,
+                                            width=width,
+                                            height=height,
+                                            disaster_type='All',
+                                            json=True
+                                            )
+            return render_template("/deepdive.html",
+                                    title="Deepdive",
+                                    disaster_types=disaster_types,
+                                    chart_json=json_chart)
+        if request.method == 'POST':
+            disaster_type = request.form['disaster_type']
+            json_chart = make_deepdive_chart(domain=domain,
+                                            width=width,
+                                            height=height,
+                                            disaster_type=disaster_type,
+                                            json=True
+                                            )
+            return render_template('/deepdive.html',
+                                    title='Deepdive',
+                                    disaster_type=disaster_type,
+                                    disaster_types=disaster_types,
+                                    chart_json=json_chart)
+    except Exception as e:
+        return "Something went wrong: " + e + e.message
